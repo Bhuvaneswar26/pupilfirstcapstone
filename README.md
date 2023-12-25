324162f96aa3708ef8f758f5b9a2212db15cc360
Bhuvaneswar <21951a0527@iare.ac.in>
2023-12-25 12:46:29 +0530

    integrated testing andd precommit using jest and husky

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pupilfirstcapstone
+
 This is an capstone project in the pupilfirst wd201 course
--- app.js
+++ app.js
@@ -1,32 +1,27 @@
 /* eslint-disable no-undef */
 //BuiltIn Pacakges importing
-const  createError = require('http-errors');
-const  express = require('express');
-const  path = require('path');
-const  cookieParser = require('cookie-parser');
-const  logger = require('morgan');
-
-
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
 //Routes importing
-const  indexRouter = require('./routes/index');
-const  usersRouter = require('./routes/users');
-const  app = express();
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 //midllware setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
