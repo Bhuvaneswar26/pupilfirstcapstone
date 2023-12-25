d85f9aac1870e9236f5118a13442c90932cd5a32
Bhuvaneswar <21951a0527@iare.ac.in>
2023-12-25 11:56:59 +0530

    project setup

---
+++ app.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+//BuiltIn Pacakges importing
+const  createError = require('http-errors');
+const  express = require('express');
+const  path = require('path');
+const  cookieParser = require('cookie-parser');
+const  logger = require('morgan');
+
+
+
+//Routes importing
+const  indexRouter = require('./routes/index');
+const  usersRouter = require('./routes/users');
+const  app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+//midllware setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+module.exports = app;
