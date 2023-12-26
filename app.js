1693edc73d2da66c56b6fe6ce5798526ed6bad03
Bhuvaneswar <21951a0527@iare.ac.in>
2023-12-26 19:24:27 +0530

    login andsignup features implemented

--- app.js
+++ app.js
@@ -1,27 +1,151 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 //BuiltIn Pacakges importing
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+var flash = require("connect-flash");
+const passport = require("passport");
+const session = require("express-session");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+//importing models
+const { User } = require("./models");
+const Sequelize = require("sequelize");
 
 //Routes importing
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const signup = require("./routes/signup");
+
+//creating the app server
 const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+//using flash
+app.use(flash());
+
 //midllware setup
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+//session midllware setup
+app.use(
+  session({
+    secret: "this is the secret key for lms",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 100,
+    },
+  }),
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+console.log("Passport initialization completed successfully");
+
+app.use((err, req, res, next) => {
+  console.error("Unhandled error:", err);
+  res.status(500).send("Internal Server Error");
+});
+
+//defing the authUser function for local  strategy
+const authUser = async (mail, password, done) => {
+  try {
+    const user = await User.findOne({ where: { email: mail } });
+    if (!user) {
+      console.log("first if", user);
+      return done(null, false, { message: "User Not Found" });
+    }
+    bcrypt.compare(password, user.password, function (err, result) {
+      if (result) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "Invalid Password" });
+      }
+    });
+  } catch (err) {
+    console.error("Error in authUser:", err);
+    return done(err);
+  }
+};
+
+//defining which strategey of passportjs to use
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "mail",
+      passwordField: "password",
+    },
+    authUser,
+  ),
+);
+
+//serialing the user
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+//deserializing the user
+passport.deserializeUser((id, done) => {
+  console.log("hiii");
+  User.findByPk(id)
+    .then((user) => {
+      console.log(user);
+      done(null, user);
+    })
+    .catch((error) => {
+      done(error, null);
+    });
+});
+
+//defining the middleware function for checking the user is loged in or not
+const isLogedIn = (request, response, next) => {
+  if (request.isAuthenticated) {
+    next();
+  } else {
+    response.redirect("/login");
+  }
+};
+const logincheck = (request, response) => {
+  if (request.isAuthenticated) {
+    response.redirect("/home");
+  } else {
+    response.redirect("/login");
+  }
+};
+
+//defining the routes
+app.get("/login", isLogedIn, (request, response) => {
+  response.render("login", { error: request.flash("error") });
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  (req, res) => {
+    console.log("test user", req.user);
+    res.send("hiii");
+  },
+);
+
+app.use("/signup", signup);
+
+app.use("/", logincheck);
+
+//exporting the app
 module.exports = app;
---
+++ controllers/signup.js
@@ -0,0 +1,68 @@
+const User = require("../models").User;
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const getsignup = (request, response) => {
+  response.render("signup");
+};
+
+const postsignup = async (request, response) => {
+  console.log("test controller");
+  console.log(request.body);
+
+  if (!request.body.email) {
+    request.flash("error", "Email is required");
+    return response.render("signup", { error: request.flash("error") });
+  }
+  if (!request.body.password) {
+    request.flash("error", "Password is required");
+    return response.render("signup", { error: request.flash("error") });
+  }
+  if (!request.body.firstname) {
+    request.flash("error", "First Name is required");
+    return response.render("signup", { error: request.flash("error") });
+  }
+  if (!request.body.role) {
+    request.flash("error", "Role is required");
+    return response.render("signup", { error: request.flash("error") });
+  }
+
+  try {
+    let hashedPassword;
+    await bcrypt.hash(
+      request.body.password,
+      saltRounds,
+      async function (err, hash) {
+        if (hash) {
+          hashedPassword = hash;
+          await User.create({
+            firstName: request.body.firstname,
+            lastName: request.body.lastname,
+            email: request.body.email,
+            password: hashedPassword,
+            role: request.body.role,
+          });
+          response.redirect("/login");
+        } else {
+          request.flash("error", "Password is required");
+          return response.render("signup", { error: request.flash("error") });
+        }
+      },
+    );
+  } catch (err) {
+    if (err.name === "SequelizeValidationError") {
+      const errorMessages = err.errors.map((error) => error.message);
+      request.flash("error", errorMessages);
+      return response.render("signup", { error: request.flash("error") });
+    } else if (err.name === "SequelizeUniqueConstraintError") {
+      request.flash("error", "User Already Exists");
+      return response.render("signup", { error: request.flash("error") });
+    } else {
+      console.error(err);
+      request.flash("error", "An unexpected error occurred");
+      return response.render("signup", { error: request.flash("error") });
+    }
+  }
+};
+
+module.exports = { getsignup, postsignup };
